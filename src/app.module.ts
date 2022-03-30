45f1fe65e0b1e25dcaf6c4ff50caf2ae0697da24
Vasyl <vasyaboychuk@gmail.com>
2022-03-31 01:15:05 +0300

    lesson typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,21 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { LessonModule } from './lesson/lesson.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
     }),
     LessonModule,
   ],
--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,8 +1,13 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Inject } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 @Resolver(() => LessonType)
 export class LessonResolver {
+  @Inject()
+  private lessonService: LessonService;
+
   @Query(() => LessonType)
   lesson() {
     return {
@@ -11,5 +16,14 @@ export class LessonResolver {
       startDate: new Date().toISOString(),
       endDate: new Date().toISOString(),
     };
+  }
+
+  @Mutation(() => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
